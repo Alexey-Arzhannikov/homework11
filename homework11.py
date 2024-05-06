c9fab1fb1a38834fa3794075425a2009e0f25935
Alexey-Arzhannikov <168473325+Alexey-Arzhannikov@users.noreply.github.com>
2024-05-06 20:14:10 +0300

    Add files via upload

---
+++ homework11.py
@@ -0,0 +1,15 @@
+def test():
+    a = [1, 3, 5, 7, 9]
+    b = [0, 2, 4, 6, 8]
+    print(a, '- нечетные числа')
+    print(b, '- четные числа')
+
+test()
+
+def test2(First_name='Ivan', Last_name='Ivanov', City='Woscow'):
+    print("Имя: ", First_name)
+    print("Фамилия: ", Last_name)
+    print("Город: ", City)
+
+
+test2()
